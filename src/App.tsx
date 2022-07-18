8f1ee6dc394d0b06297c593fc3c3f4c851f75f0b
Kamil Balcerzak <kbalcerek@o2.pl>
2022-07-18 11:56:13 +0200

    - created Board - added click (make move) functionalities - added checkWinner (not finished)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const initialCleanBoard: string[] = ['', '', '', '', '', '', '', '', ''];
+
 function App() {
+  const [board, setBoard] = useState<string[]>([...initialCleanBoard]);
+  const [player, setPlayer] = useState('O'); // TODO: Change 'O' and 'X' to constants
+  const [winner, setWinner] = useState('');
+
+  const checkWinner = () => {
+    ['X' , 'O'].forEach((item) => {
+      // TODO: refactor
+      if (board[0] === item && board[1] === item && board[2] === item){
+        setWinner(item);
+        return;
+      }
+      if (board[3] === item && board[4] === item && board[5] === item){
+        setWinner(item);
+        return;
+      }
+      if (board[0] === item && board[1] === item && board[2] === item){
+        setWinner(item);
+        return;
+      }
+    })
+  }
+
+
+  const startGame = () => {
+    setBoard([...initialCleanBoard]);
+  }
+
+  const cellClick = (index: number) => () => {
+    board[index] = player;
+    setPlayer(player === "O" ? 'X' : 'O')
+    setBoard(board);
+    checkWinner();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="board">
+        {board.map((c, index) => (
+          <div
+            id={`cell${index}`}
+            className="cell"
+            onClick={cellClick(index)}
+          >{board[index]}</div>
+        ))}
+      </div>
+
+      {winner !== '' && (<p id="message">Winner is {winner}</p>)}
+      
+      <button id="restartGame" onClick={startGame}>Restart Game</button>
     </div>
   );
 }
